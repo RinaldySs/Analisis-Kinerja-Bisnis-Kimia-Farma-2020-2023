e90a2f389d3021008f74732983a1eb93b25a0c44
Rinaldy Buana Saputra <73944208+RinaldySs@users.noreply.github.com>
2024-04-04 13:44:26 +0700

    Tabel_analisa.sql

--- Tabel_analisa.sql
+++ Tabel_analisa.sql
@@ -36,5 +36,5 @@ SELECT
     END AS nett_profit,
 f.rating as rating_transaksi -- Alias for rating of the transaction
 FROM kimia_farma.kf_final_transaction AS f
-Left kimia_farma.kf_kantor_cabang k ON f.branch_id = k.branch_id       
-Left kimia_farma.kf_product p ON f.product_id = p.product_id;
+LEFT JOIN kimia_farma.kf_kantor_cabang k ON f.branch_id = k.branch_id       
+LEFT JOIN kimia_farma.kf_product p ON f.product_id = p.product_id;
